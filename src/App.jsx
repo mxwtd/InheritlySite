db7c5cec343092f55fc71e081d163053c23fbeb6
max whitehead <fifes-catkin.0r@icloud.com>
2023-05-31 16:03:54 +0100

    Modified Home screen layout 2

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <div className='bg-white border-slate-200 dark:bg-slate-900'>
       <Router>
         <Navbar />
-        <div className='mx-0 mt-0 md:mx-12 md:mt-2 md:rounded-3xl overflow-hidden'>
+        <div className='mx-0 md:mx-12 md:rounded-3xl overflow-hidden'>
           <div className="w-full h-[100%] bg-[url('https://res.cloudinary.com/djr22sgp3/image/upload/v1683277801/Light_ekxcia.svg')] dark:bg-[url('https://res.cloudinary.com/djr22sgp3/image/upload/v1683278025/Dark_b0r36s.svg')] bg-cover bg-center bg-no-repeat">
             <Routes>
               <Route path="/about" element={<About />} />
