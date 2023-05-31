85284f9992e9ed43ed9a0b2ea557fc46dc63f5ce
max whitehead <fifes-catkin.0r@icloud.com>
2023-05-31 14:40:04 +0100

    Added Features Container 3

--- src/assets/components/HomeFeatures.jsx
+++ src/assets/components/HomeFeatures.jsx
@@ -26,7 +26,7 @@ const HomeFeatures = () => {
             <div>
               <h1 className="mb-3 text-3xl md:text-5xl font-extrabold tracking-tight text-slate-900 dark:text-white">Features</h1>
               <p className="mb-3 text-slate-500 dark:text-slate-400">A wide array of features to help you track and view your family assets globally.</p>
-              <div className='grid grid-cols-2 lg:grid-cols-4 justify-items-center md:px-20 pt-5'>
+              <div className='grid grid-cols-2 lg:grid-cols-4 justify-items-center pt-10'>
                 <div className='flex flex-col justify-center items-center gap-3 mb-4'>
                   <svg fill="none" className='w-20 h-20 text-slate-500 dark:text-slate-400' stroke="currentColor" strokeWidth={1} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z" />
