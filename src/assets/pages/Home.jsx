ac8b9e77b339b36b214f8a4f20f9b52b65b67bd3
max whitehead <fifes-catkin.0r@icloud.com>
2023-05-31 14:37:42 +0100

    Added Features Container

--- src/assets/pages/Home.jsx
+++ src/assets/pages/Home.jsx
@@ -1,10 +1,12 @@
 import HomeHero from "../components/HomeHero"
 import HomeCards from "../components/HomeCards"
+import HomeFeatures from "../components/HomeFeatures"
 
 const Home = () => {
   return (
     <>
       <HomeHero />
+      <HomeFeatures />
       <HomeCards />
     </>
   )
