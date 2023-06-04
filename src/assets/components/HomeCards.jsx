7cd7c86d14b8b13ae6ddb901a471628397c6fd0f
max whitehead <fifes-catkin.0r@icloud.com>
2023-06-04 12:28:46 +0200

    Updated responsiveness 2 - homeScreen

--- src/assets/components/HomeCards.jsx
+++ src/assets/components/HomeCards.jsx
@@ -36,9 +36,9 @@ const HomeCards = () => {
               <p className="bg-blue-100 text-blue-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded-md dark:bg-slate-600 dark:text-blue-400 mb-4">
                 Explore
               </p>
-              <h1 className="text-slate-900 dark:text-white text-3xl md:text-5xl font-extrabold mb-3">Your family assets, visualised.</h1>
-              <p className="text-lg font-normal text-slate-500 dark:text-slate-400 mb-6">Our fintech app empowers you to effortlessly manage your family estate. Gain full control over your assets, streamline planning, and make informed decisions. Unlock financial opportunities, collaborate with ease, and secure a prosperous future. Simplify wealth management and embrace the future of estate planning with our intuitive platform.</p>
-              <a href="#" className="inline-flex justify-center items-center py-2.5 px-5 text-base font-medium text-center text-white rounded-2xl bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+              <h1 className="text-slate-900 dark:text-white text-2xl md:text-5xl font-extrabold mb-3">Your family assets, visualised.</h1>
+              <p className="text-sm md:text-lg font-normal text-slate-500 dark:text-slate-400 mb-6">Our fintech app empowers you to effortlessly manage your family estate. Gain full control over your assets, streamline planning, and make informed decisions. Unlock financial opportunities, collaborate with ease, and secure a prosperous future. Simplify wealth management and embrace the future of estate planning with our intuitive platform.</p>
+              <a href="#" className="inline-flex justify-center items-center py-2.5 px-5 text-sm md:text-lg font-medium text-center text-white rounded-2xl bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
                 Explore Features
                 <svg aria-hidden="true" className="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
               </a>
@@ -55,9 +55,9 @@ const HomeCards = () => {
               <p className="bg-green-200 text-green-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded-md dark:bg-slate-600 dark:text-green-400 mb-3">
                 AI Generator
               </p>
-              <h2 className="text-slate-900 dark:text-white text-3xl font-extrabold mb-3">Effortless Will creation with AI</h2>
-              <p className="text-lg font-normal text-slate-500 dark:text-slate-400 mb-4">Our app features an AI-powered will generator for seamless estate planning. Say goodbye to complex paperwork and hello to a simplified process. Experience the convenience of creating a comprehensive will with ease, backed by intelligent algorithms. Secure the future of your estate effortlessly and efficiently.</p>
-              <a href="#" className="text-blue-600 dark:text-blue-500 hover:underline font-medium text-lg inline-flex items-center">Try the Will Generator
+              <h2 className="text-slate-900 dark:text-white text-2xl font-extrabold mb-3">Effortless Will creation with AI.</h2>
+              <p className="text-sm md:text-lg font-normal text-slate-500 dark:text-slate-400 mb-4">Our app features an AI-powered will generator for seamless estate planning. Say goodbye to complex paperwork and hello to a simplified process. Experience the convenience of creating a comprehensive will with ease, backed by intelligent algorithms. Secure the future of your estate effortlessly and efficiently.</p>
+              <a href="#" className="text-blue-600 dark:text-blue-500 hover:underline font-medium text-sm md:text-lg inline-flex items-center">Try the Will Generator
                 <svg aria-hidden="true" className="w-4 h-4 ml-2" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M17 8l4 4m0 0l-4 4m4-4H3"></path>
                 </svg>
@@ -74,9 +74,9 @@ const HomeCards = () => {
               <p className="bg-purple-100 text-purple-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded-md dark:bg-slate-600 dark:text-purple-400 mb-3">
                 Simple
               </p>
-              <h2 className="text-slate-900 dark:text-white text-3xl font-extrabold mb-3">Family-Friendly Simplicity</h2>
-              <p className="text-lg font-normal text-slate-500 dark:text-slate-400 mb-4">Collaborate effortlessly with our app, designed to simplify wealth management for the entire family. Seamlessly track assets, share documents, and make decisions together. Enjoy a user-friendly interface that promotes transparency and strengthens family unity. Experience hassle-free collaboration while securing a prosperous future.</p>
-              <a href="#" className="text-blue-600 dark:text-blue-500 hover:underline font-medium text-lg inline-flex items-center">Try it Free
+              <h2 className="text-slate-900 dark:text-white text-2xl font-extrabold mb-3">Family-Friendly Simplicity</h2>
+              <p className="text-sm md:text-lg font-normal text-slate-500 dark:text-slate-400 mb-4">Collaborate effortlessly with our app, designed to simplify wealth management for the entire family. Seamlessly track assets, share documents, and make decisions together. Enjoy a user-friendly interface that promotes transparency and strengthens family unity. Experience hassle-free collaboration while securing a prosperous future.</p>
+              <a href="#" className="text-blue-600 dark:text-blue-500 hover:underline font-medium text-sm md:text-lg inline-flex items-center">Try it Free
                 <svg aria-hidden="true" className="w-4 h-4 ml-2" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M17 8l4 4m0 0l-4 4m4-4H3"></path>
                 </svg>
--- src/assets/components/HomeHero.jsx
+++ src/assets/components/HomeHero.jsx
@@ -30,11 +30,11 @@ const HomeHero = () => {
               </div>
               <div id='column2' className="flex-1 text-left px-10">
                 <a href="#" className="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-blue-700 bg-blue-200/60 rounded-full dark:bg-blue-900 dark:text-blue-300 hover:bg-blue-200 dark:hover:bg-blue-800">
-                  <span className="text-xs bg-blue-600 rounded-full text-white px-4 py-1.5 mr-3">New</span> <span className="text-sm font-medium">Dashboard launched</span>
+                  <span className="text-xs bg-blue-600 rounded-full text-white px-4 py-1.5 mr-3">New</span> <span className="text-xs md:text-sm font-medium">Dashboard</span>
                   <svg aria-hidden="true" className="ml-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd"></path></svg>
                 </a>
-                <h1 className="mb-4 text-5xl font-extrabold text-slate-900 dark:text-white lg:text-6xl"><span className="text-transparent bg-clip-text bg-gradient-to-b to-teal-400 from-blue-700 dark:to-white dark:from-blue-700">Your Assets,</span><br />in one place.</h1>
-                <p className="mb-8 text-lg font-normal text-slate-500 lg:text-xl dark:text-slate-200">At Inheritly, we concentrate on creating a seamless digital platform where technology and comprehensive asset management meet. Our goal is to transform estate planning and wealth tracking, driving effective long-term wealth growth for families.</p>
+                <h1 className="mb-4 text-3xl md:text-5xl font-extrabold text-slate-900 dark:text-white lg:text-6xl"><span className="text-transparent bg-clip-text bg-gradient-to-b to-teal-400 from-blue-700 dark:to-white dark:from-blue-700">Your Assets,</span><br />in one place.</h1>
+                <p className="mb-8 text-sm md:text-lg font-normal text-slate-500 lg:text-xl dark:text-slate-200">At Inheritly, we concentrate on creating a seamless digital platform where technology and comprehensive asset management meet. Our goal is to transform estate planning and wealth tracking, driving effective long-term wealth growth for families.</p>
               </div>
             </div>
           </motion.div>
