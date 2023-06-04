c3be3f74bc9d8cd5731ef74f1fe14fe5d5191f82
max whitehead <fifes-catkin.0r@icloud.com>
2023-06-04 17:52:41 +0200

    Changed home responsiveness 2

--- src/assets/components/AboutBanner.jsx
+++ src/assets/components/AboutBanner.jsx
@@ -20,7 +20,7 @@ const AboutBanner = () => {
           >
             <div className='flex flex-col justify-start items-left h-full'>
               <h1 className="mb-4 text-2xl font-extrabold text-slate-900 dark:text-white lg:text-3xl"><span className="text-transparent bg-clip-text bg-gradient-to-b to-teal-400 from-blue-700 dark:to-white dark:from-blue-700">Our Vision</span></h1>
-              <p className=" text-lg font-normal text-slate-500 lg:text-xl dark:text-slate-200">To revolutionise family estate wealth management, empowering families to secure their financial legacies with confidence, while providing a seamless user experience.</p>
+              <p className="text-sm md:text-lg font-normal text-slate-500 dark:text-slate-200">To revolutionise family estate wealth management, empowering families to secure their financial legacies with confidence, while providing a seamless user experience.</p>
             </div>
           </motion.div>
           <motion.div
@@ -32,7 +32,7 @@ const AboutBanner = () => {
           >
             <div>
               <h1 className="mb-4 text-2xl font-extrabold text-slate-900 dark:text-white lg:text-3xl"><span className="text-transparent bg-clip-text bg-gradient-to-b to-teal-400 from-blue-700 dark:to-white dark:from-blue-700">Our Mission</span></h1>
-              <p className="mb-8 text-lg font-normal text-slate-500 lg:text-xl dark:text-slate-200">At Inheritly, our mission is to empower families to confidently navigate and manage their wealth, ensuring a lasting legacy for future generations. We strive to simplify the complexities of estate management through our innovative app, providing accessible tools and resources that promote transparency, collaboration, and informed decision-making. By leveraging technology and fostering a user-centric approach, we aim to revolutionise the way families preserve and grow their financial assets.</p>
+              <p className=" text-sm md:text-lg font-normal text-slate-500 dark:text-slate-200">At Inheritly, our mission is to empower families to confidently navigate and manage their wealth, ensuring a lasting legacy for future generations. We strive to simplify the complexities of estate management through our innovative app, providing accessible tools and resources that promote transparency, collaboration, and informed decision-making. By leveraging technology and fostering a user-centric approach, we aim to revolutionise the way families preserve and grow their financial assets.</p>
             </div>
           </motion.div>
         </div>
